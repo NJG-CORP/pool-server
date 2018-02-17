e6c41a6f9823c5275386b57a6a92cb179d74b171
tooyz <tooy_m@mail.ru>
2018-02-17 16:52:55 +0300

    add city docs

--- app/Models/Image.php
+++ app/Models/Image.php
@@ -8,8 +8,14 @@ class Image extends Model
 {
     public $timestamps = false;
     protected $dates = ['created_at'];
+    protected $hidden = ['id', 'imageable_id', 'imageable_type', 'path'];
+    protected $appends = ['url'];
 
     public function imageable(){
         return $this->morphTo();
+    }
+
+    public function getUrlAttribute(){
+        return "/public/assets/images" . $this->path;
     }
 }
---
+++ docs/cities.md
@@ -0,0 +1,17 @@
+**Получить свой профиль**
+
+**GET /cities/search**
+```
+    >query!
+```
+```
+    {
+        cities: [
+            {
+                id: number,
+                name: string,
+                type: string
+            }
+        ]
+    }
+```
