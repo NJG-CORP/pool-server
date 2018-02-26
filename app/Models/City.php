67e870f14a92ddefd7280ea8a839dd6c904b0dee
tooyz <tooy_m@mail.ru>
2018-02-26 19:44:07 +0300

    fix

--- app/Models/City.php
+++ app/Models/City.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class City extends Model
 {
     public $timestamps = false;
+    protected $guarded = [];
 
     public function users(){
         return $this->hasMany(User::class);
