4c59bb20af127d87f69d60fb1e02a083efde74f5
tooyz <tooy_m@mail.ru>
2018-03-03 14:53:43 +0300

    fix image path

--- app/Services/ImageService.php
+++ app/Services/ImageService.php
@@ -14,7 +14,8 @@ class ImageService
      */
     public function create($b64, Model $model = null, $path){
         $public = public_path();
-        $imagePath = $public . "/assets/images";
+        $assetsImagesPath = "/assets/images";
+        $imagePath = $public . $assetsImagesPath;
         $imagePath = $imagePath . '/' . $path;
         $imageDir = dirname($imagePath);
         if ( !is_dir($imageDir) ){
@@ -28,7 +29,7 @@ class ImageService
             $image = ImageModel::create([
                 "imageable_id" => $model->id,
                 "imageable_type" => get_class($model),
-                "path" => str_replace($public, '', $imagePath)
+                "path" => str_replace($public . $assetsImagesPath, '', $imagePath)
             ]);
             return $image;
         }
