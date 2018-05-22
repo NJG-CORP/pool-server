425d56c654c75ab26d62b8e6ddf8b4147c1af065
Tooyz <tooy_m@mail.ru>
2018-05-22 17:11:05 +0300

    re

--- app/Services/DeviceService.php
+++ app/Services/DeviceService.php
@@ -14,7 +14,7 @@ class DeviceService
      * @return Device
      */
     public function ensureDevice(User $user, $deviceToken, $platform, $playerId){
-        $device = Device::where('device_token', $deviceToken)->first();
+        $device = Device::where('player_id', $playerId)->first();
         if ( $device ) return $device;
         return Device::create([
             'user_id' => $user->id,
