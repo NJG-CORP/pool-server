7ce5200f595107047634c7f462466381fe7128c7
Tooyz <tooy_m@mail.ru>
2018-05-29 19:54:19 +0300

    re

--- app/Services/PlayerService.php
+++ app/Services/PlayerService.php
@@ -146,7 +146,7 @@ class PlayerService
 
         if ( $avatar ){
             \DB::delete("
-              DELETE FROM images WHERE imageable_type = 'App\\Models\\User'
+              DELETE FROM images WHERE imageable_type = 'App\\\\Models\\\\User'
               AND imageable_id = " . $user->id);
             $imagePath = "avatars/" . str_random(8) . '.jpg';
             $imageService->create(
